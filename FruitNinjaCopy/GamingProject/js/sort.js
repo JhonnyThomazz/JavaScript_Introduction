868e62974e942ffaec680c2eaa12b406dee57fec
Here's Jhonny! <jhonnythomazz@gmail.com>
2026-02-24 16:30:29 -0300

    FruitNinjaInDevelopment

--- FruitNinjaCopy/GamingProject/js/sort.js
+++ FruitNinjaCopy/GamingProject/js/sort.js
@@ -1,19 +1,33 @@
 const btn = document.getElementById("btn");
+const obj = document.createElement("div");
+
+obj.addEventListener("click", ()=>{
+ obj.remove
+});
 
 btn.addEventListener("click", ()=>{
-    const obj = document.createElement("div");
-
+    const pause =setInterval(() => {
+        
     obj.style.width ="100px";
     obj.style.height ="100px";
     obj.style.backgroundColor ="red";
     obj.style.position = "absolute";
     obj.style.left ="100px";
 
-    let x = Math.random()*window.innerWidth -5;
-    let y = Math.random()*window.innerHeight -5;
+    let x = Math.random()*window.innerWidth -150;
+    let y = Math.random()*window.innerHeight -150;
 
     obj.style.left = x + "px";
     obj.style.top = y + "px";
 
     document.body.appendChild(obj);
+    }, 1000);
+    
+   
+   btn2.addEventListener("click", ()=>{
+    clearInterval(pause);
+    obj.remove();
 });
+
+});
+
